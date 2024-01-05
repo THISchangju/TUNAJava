a0fa2c94180afdd126e15afb806f76e5e5e975c7
THISchangju <changju0226@gmail.com>
2024-01-05 15:03:23 +0900

    1. 절차지향 프로그래밍부터 객체지향 프로그래밍까지 실습   - ScoreProgram1,2,3 2. 객체배열을 활용한 객체지향 프로그래밍   - Exam_ObjectArray1, 2.java(Circle, Book)   - Student, StudentApp, ManageStudent.java

---
+++ src/org/TUNA/day06/oop/score/objectarray/Book.java
@@ -0,0 +1,13 @@
+package org.TUNA.day06.oop.score.objectarray;
+
+public class Book {
+	public String title;
+	public String author;
+	
+	public Book() {}
+	
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+}
