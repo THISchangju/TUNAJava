263069e40bf9b77d18de5c11ae476807af9b86a3
THISchangju <changju0226@gmail.com>
2024-01-09 16:43:59 +0900

    [ADD] 23/12/27 자바 4일차, 정렬 알고리즘 및 2차원 배열
    1. 배열을 활용한 정렬 알고리즘
     - 삽입정렬
     - 선택정렬
     - 버블정렬
    2. 2차원 배열 개념 및 실습예제
     - 2차원 배열에 값 입력 예제

--- src/org/TUNA/day04/exercise/Exercise_DimArray3.java
+++ src/org/TUNA/day04/exercise/Exercise_DimArray3.java
@@ -2,32 +2,24 @@ package org.TUNA.day04.exercise;
 
 public class Exercise_DimArray3 {
 	public static void main(String[] args) {
-		// 1 2 3 4 5
-		// num[0][0], num[0][1], num[0][2], num[0][3], num[0][4] 
-		// 10 9 8 7 6
-		// num[1][4], num[1][3], num[1][2], num[1][1], num[1][0] 
-		// 11 12 13 14 15
-		// num[2][0], num[2][1], num[2][2], num[2][3], num[2][4] 
-		// 20 19 18 17 16
-		// num[3][4], num[3][3], num[3][2], num[3][1], num[3][0] 
-		// 21 22 23 24 25
-		// num[4][0], num[4][1], num[4][2], num[4][3], num[4][4] 
-		int arrs [][] = new int [5][5];
-		int num = 1;
-		for(int i = 0; i < arrs.length; i++) {
-			if(i % 2 == 0) {
-				for(int j = 0; j < arrs[i].length; j++) {
-					arrs[i][j] = num++;								
-				}
-			}else {
-				for(int j = arrs.length-1; j >= 0; j--) {
-					arrs[i][j] = num++;
-				}
+		// 1 6 11 16 21
+		// 2 7 12 17 22
+		// 3 8 13 18 23
+		// 4 9 14 19 24
+		// 5 10 15 20 25
+		//nums[0][0],nums[1][0], nums[2][0], nums[3][0]
+		int nums [][] = new int[5][5];
+		int k = 1;
+		// 이차원배열 데이터 입력
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j < nums[i].length; j++) {
+				nums[j][i] = k++;
 			}
 		}
-		for(int i = 0; i < arrs.length; i++) {
-			for(int j = 0; j < arrs[i].length; j++) {
-				System.out.print(arrs[i][j] + "\t");
+		// 이차원 배열 출력
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j < nums[i].length; j++) {
+				System.out.print(nums[i][j] + "\t");
 			}
 			System.out.println();
 		}
